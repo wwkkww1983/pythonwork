42ef0467168d22ae41251535b947fadb9650c025
fanch-luck <fch.luck@qq.com>
2017-06-15 23:58:01 +0800

    初步完成calibration.py的cali_ntc函数，待与研发确认过后再完善及继续后续函数的完成

--- plc_Product_tooling/ver4_0/calibration.py
+++ plc_Product_tooling/ver4_0/calibration.py
@@ -20,7 +20,7 @@ class CALIBRATION(object):
         self.expected_board = self.get_board('8TC')  # 这里“8TC”信息由UI设置中获取
         self.bench_resistance = self.get_bench_resistance()
         self.busyflag = None
-        self.cominterval = 0.1
+        self.comintervaltime = 0.1
         self.check_value = 0
         self.cali_parameter = 0
         self.varify_value = 0
@@ -56,70 +56,159 @@ class CALIBRATION(object):
     def read_board(self):
         pass
 
-    def write_board(self):
+    def write_board(self, params):
+        return False
+
+    def switch_channel(self):
         pass
 
-    def set_channel(self):
+    def switch_res(self):
         pass
 
-    def determinate_res(self):
+    def get_res_realvalue(self):
         """
-        检测电阻，可能通过不同方式
+        获取电阻真实值，可能通过不同方式，需要滤波？设置增益，
         :return:
         """
-        initvalue = 0  #res检测初始值
-        return initvalue
+        realvalue = 0  #res检测初始值
+        return realvalue
 
-    def calc_ntc(self):
+    def get_res_feedbkvalue(self):
         """
-        ntc校准
+        获取电阻检测值，通过board，需要滤波？设置增益
+        :return:
+        """
+        realvalue = 0  #res检测初始值
+        return realvalue
+
+    def cali_ntc(self):
+        """
+        ntc校准,a,b,c,d分别代表4组电阻检测：校准1，校准2，检验1，检验2
         :param chl_num: 通道数
-        :param bchmk: 基准参数
+        :param bchmk:
         :return: 校准数据，误差，通过标识
         """
+        if self.busyflag is not False:
+            log.error('调用函数 {} 时资源使用冲突，请检查'.format(__name__))
+            return
+        else:
+            pass
+        channelnum = self.expected_board.channel_quantity
+        channellist = list(range(1, 1+channelnum)) # 通道从1开始算
         benchress = self.bench_resistance
-        initvalue_a = 0
-        initvalue_b = 0
-        calires_a = benchress[0]
-        calires_b = benchress[1]
+        # 存放ntc校准的参数、误差、当前值, 用于显示
+        ntc_cali_params = [0] * channelnum  # 最终结果可能是个tuple列表
+        ntc_cali_errors = [0] * channelnum
+        ntc_current_values = [0] * channelnum
+        ntc_calipass = [False] * channelnum
+
+        res_a = benchress[0]
+        res_b = benchress[1]
+        res_c = benchress[2]
+        res_d = benchress[3]
+        initvalue_res_a = 0
+        initvalue_res_b = 0
+        initvalue_res_c = 0
+        initvalue_res_d = 0
+        realvalue_res_a = 0
+        realvalue_res_b = 0
+        realvalue_res_c = 0
+        realvalue_res_d = 0
         caliparam_a = 0
         caliparam_b = 0
-        checkres_a = benchress[2]
-        checkres_b = benchress[3]
-        calierror_a = 0
-        calierror_b = 0
+        calierror_c = 0
+        calierror_d = 0
+
+        def calc_param(rra,ira,rrb,irb):
+            parama = func(rra,ira)
+            paramb = func(rrb,irb)
+            return parama,paramb
+
+        def calc_error(rrc,irc,rrd,ird):
+            return errorfunc(rrc,irc,rrd,ird)
+
         if self.busyflag is not None:
-            log.info('校准程序忙')
+            log.info('校准程序忙，等待响应...')
         else:
-            for channel in range(self.expected_board.channel_quantity):
-                self.set_channel(channel)
-                time.sleep(self.cominterval)
-                initvalue_a = self.determinate_res(calierror_a)
-                time.sleep(self.cominterval)
+            # 每个通道依次校准，并在一次循环中尽量保留所有信息，数据处理到位。不要多次循环
+            for channel in channellist:  # 每个通道依次检测
+                self.switch_channel(channel)  # 设定当前通道，一次只能设置1个通道为开
+
+                # 获取校准电阻a,b的真实值、回显值
+                time.sleep(self.comintervaltime)  # 操作间隔（硬件切换时间和通讯占用），下同
+                realvalue_res_a = self.get_res_realvalue(res_a)
+                time.sleep(self.comintervaltime)
+                initvalue_res_a = self.get_res_feedbkvalue(res_a)
+                time.sleep(self.comintervaltime)
+                realvalue_res_b = self.get_res_realvalue(res_b)
+                time.sleep(self.comintervaltime)
+                initvalue_res_b = self.get_res_feedbkvalue(res_b)
+
+                # 根据4组值，计算校准参数并写入被测板
+                if realvalue_res_a & initvalue_res_a & realvalue_res_b & initvalue_res_b is None:
+                    log.error('NTC校准过程获取电阻值错误，通道={}, 阻值={}'.format(
+                        channel, [realvalue_res_a, initvalue_res_a, realvalue_res_b, initvalue_res_b]))
+                    break
+                else:
+                    params = calc_param(realvalue_res_a,initvalue_res_a,realvalue_res_b,initvalue_res_b)
+                    ntc_cali_params[channel-1] = params  # 每个通道将相应校准参数整合到一个列表中
+                    writed = self.write_board(*params)
+                    time.sleep(self.comintervaltime)
+                    if writed is False:
+                        log.error('NTC校准过程通讯错误，通道={}, 参数={}'.format(channel, params))
+                        break
+                    else:
+                        pass
+
+                # 获取检验电阻d,c的真实值、回显值
+                realvalue_res_c = self.get_res_realvalue(res_c)  # 获取真实值，通过红外模块？下同
+                time.sleep(self.comintervaltime)
+                initvalue_res_c = self.get_res_feedbkvalue(res_c)  # 获取检测值，通过被测板，下同
+                time.sleep(self.comintervaltime)
+                realvalue_res_d = self.get_res_realvalue(res_d)
+                time.sleep(self.comintervaltime)
+                initvalue_res_d = self.get_res_feedbkvalue(res_d)
+
+                # 根据4组值，计算该通道误差，并显示。
+                if realvalue_res_c & initvalue_res_c & realvalue_res_d & initvalue_res_d is None:
+                    log.error('NTC检验过程获取电阻值错误，通道={}, 阻值={}'.format(
+                        channel, [realvalue_res_c, initvalue_res_c, realvalue_res_d, initvalue_res_d]))
+                    break
+                else:
+                    error = calc_error(realvalue_res_c,initvalue_res_c,realvalue_res_d,initvalue_res_d)
+                    ntc_cali_errors[channel-1] = error
+
+
+        if ntc_calipass != [True]*channelnum:
+            log.info('NTC 校准失败，通过情况：通道{}=误差{}={}'.format(channellist, ntc_cali_errors, ntc_calipass))
+        else:
+            log.info('NTC 校准通过，通道{}=误差{}'.format(channellist, ntc_cali_errors))
+
+        return ntc_calipass, ntc_cali_errors
 
 
 
-
-    def calc_tcv(self):
+    def cali_tcv(self):
         pass
 
-    def calc_tcr(self):
+    def cali_tcr(self):
         pass
 
-    def calc_pt(self):
+    def cali_pt(self):
         pass
 
-    def calc_adi(self):
+    def cali_adi(self):
         pass
 
-    def calc_dai(self):
+    def cali_dai(self):
         pass
 
-    def calc_adv(self):
+    def cali_adv(self):
         pass
 
-    def calc_dav(self):
+    def cali_dav(self):
         pass
+
 
 def main():
     # b = board()
