965b9d17dd3bd5b7e0a7a9fac9d142089463bc5f
fanch-luck <fch.luck@qq.com>
2017-06-20 18:36:00 +0800

    USB HID调试，用demo2viaHID小项目 调试断点，借鉴其用法

--- local_projects/pywinusb/usb_test.py
+++ local_projects/pywinusb/usb_test.py
@@ -7,36 +7,38 @@
 import sys
 import pywinusb.hid as hid
 
-target_product_name = 'RAPOO GAMEPAD-X (Controller)'
-target_product_id = 0x045e
-target_vendor_id = 0x028e
-# 雷柏X-Box手柄UID设备
+target_product_name = 'PLC USB HID VER1'
+target_product_id = 0x5750
+target_vendor_id = 0x0483
+digit_link_name = 'DIGITAL MODULE VER1'
+analog_link_name = 'ANALOG MODULE VER1'
+plc_link_name = 'PLC USB HID VER1'
+# 维控USB HID
 
-
-def find_hid(name='', pid=0x0, vid=0x0):
+def find_hids(pid=0x0, vid=0x0):
     # 从系统中找到目标设备，通过product_name, product_id, vendor_id
-    all_devices = hid.HidDeviceFilter(product_name=name).get_devices()
+    all_devices = hid.HidDeviceFilter(product_id=pid, target_vendor_id=vid).get_devices()
     # all_devices = hid.HidDeviceFilter(product_id=pid, vendor_id=vid).get_devices()
     # hid设备筛选器通过product_id, vendor_id选中设备
-
+    devices = []
     if not all_devices:
-        error = '未找到HID设备(name={0}, pid={0}, vid={1})'.format(name, pid, vid)
+        error = '未找到HID设备(pid={0}, vid={1})'.format(pid, vid)
         print(error)
     else:
-        device = all_devices[0]
-        if not device:
-            print(u'已找到名为%s的USB HID设备，但无法使用' % target_product_name)
-        else:
-            print(device)
-            return device
+        for device in all_devices:
+            if device.product_name == digit_link_name:
+                digit_board = device
+            elif device.product_name == analog_link_name:
+                analog_board = device
+            else:
+                common_plc = device
+        return {'digit board': digit_board, 'anolog board': analog_board, 'common plc': common_plc}
 
-device = find_hid(target_product_name, target_product_id, target_vendor_id)
+hid_devices = find_hids(target_product_id, target_vendor_id)
 
 
-def read(data):
-    print([hex(item).upper() for item in data[1:]])
 
-if device:
-    while not input('\n'):
-        device.set_raw_data_handler(read)
+if __name__ == '__main__':
+    for name in hid_devices:
+        print('找到设备{0}:\n {1}'.format(name, hid_devices[name]))
 
