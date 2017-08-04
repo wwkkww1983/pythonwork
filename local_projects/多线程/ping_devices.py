f5cf9d19404a4377dadb05ac8bb67eb45113ebf0
fanch-luck <fch.luck@qq.com>
2017-08-04 18:47:07 +0800

    usb hid线程分离确认，进行完善

--- local_projects/多线程/ping_devices.py
+++ local_projects/多线程/ping_devices.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# python3.4 pingIP.py
-
+# !user/bin/python3
+# -*- coding: GBK -*-
 import platform
 import sys
 import os
@@ -9,11 +8,13 @@ import _thread as thread
 from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import QWidget, QGridLayout, QTextBrowser, QLabel, QApplication
 ip_addresses = []
+ping_result_data = []
+ping_result_time = []
 
 
 def get_os():
     """
-    get os ç±»å‹
+    get os ÀàĞÍ
     """
     os = platform.system()
     if os == "Windows":
@@ -26,7 +27,6 @@ def ping_ip(ip_str):
     cmd = ["ping", "-{op}".format(op=get_os()),
            "1", ip_str]
     output = os.popen(" ".join(cmd)).readlines()
-     
     flag = False
     global ip_addresses
     for line in list(output):
@@ -37,46 +37,57 @@ def ping_ip(ip_str):
             break
     if flag:
         print("ip: %s is ok ***" % ip_str)
-        print(ip_str)
-    ip_addresses.append(ip_str)
+        ip_addresses.append(ip_str)
 
 
 def test_ip(ip_addr, tms=10, lenth=1024):
     """
-    å¯¹å•ä¸ªIPåœ°å€è¿›è¡Œæ£€æµ‹
-    :param ip_addr: ipåœ°å€å­—ç¬¦ä¸²
-    :param tms: å‘é€æ¬¡æ•°
-    :param lenth: åŒ…é•¿åº¦
+    ¶Ôµ¥¸öIPµØÖ·½øĞĞ¼ì²â
+    :param ip_addr: ipµØÖ·×Ö·û´®
+    :param tms: ·¢ËÍ´ÎÊı
+    :param lenth: °ü³¤¶È
     :return:
     """
+    tms = str(tms)
+    lenth = str(lenth)
     cmd = ['ping', '-{op}'.format(op=get_os()), tms, '-l', lenth, ip_addr]
     output = os.popen(''.join(cmd)).readlines()
-
-    flag = False
+    global ping_result_time
+    global ping_result_data
     for line in list(output):
         if not line:
             continue
-        elif str(line).upper().find("TTL") >= 0:
-            continue
+        elif str(line).upper().find('Êı¾İ°ü') >= 0:
+            print(line)
+            ping_result_data.append(line)
+        elif str(line).upper().find('Æ½¾ù') >= 0:
+            print(line)
+            ping_result_time.append(line)
         else:
-            pass
+            continue
 
 
 def find_ips(ip_prefix):
     """
-    ç»™å‡ºå½“å‰çš„127.0.0ï¼Œç„¶åæ‰«ææ•´ä¸ªæ®µæ‰€æœ‰åœ°å€
+    ¸ø³öµ±Ç°µÄ127.0.0£¬È»ºóÉ¨ÃèÕû¸ö¶ÎËùÓĞµØÖ·
     """
-    for i in range(1, 50):
+    for i in range(200, 255):
         ip = '%s.%s' % (ip_prefix, i)
         thread.start_new_thread(ping_ip, (ip,))
         time.sleep(0.1)
 
 
+def check_ips(ips):
+    for ip in ips:
+        thread.start_new_thread(test_ip, (ip, 10, 1024))
+        time.sleep(.5)
 
 
 class Ui(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, titlename, parent=None):
         super(Ui, self).__init__(parent)
+
+        self.title = titlename
         self.initUi()
 
     def initUi(self):
@@ -84,9 +95,8 @@ class Ui(QWidget):
         title = QLabel('Title')
         author = QLabel('Author')
         review = QLabel('Review')
-
-        titleLabel = QLabel('ä»¥å¤ªç½‘BDæ¿å‡ºå‚ç¡¬ä»¶æ£€æµ‹å·¥å…·')
-        authorLabel = QLabel('ç‰ˆæœ¬1.0ï¼Œæµ‹è¯•éƒ¨fanch')
+        titleLabel = QLabel('ÒÔÌ«ÍøBD°å³ö³§Ó²¼ş¼ì²â¹¤¾ß')
+        authorLabel = QLabel('°æ±¾1.0£¬²âÊÔ²¿fanch')
         reviwLabel = QTextBrowser()
         grid = QGridLayout()
         grid.setSpacing(10)
@@ -99,33 +109,32 @@ class Ui(QWidget):
 
         self.setLayout(grid)
         self.setGeometry(300, 300, 350, 300)
-        self.setWindowTitle('Review')
-        self.show()
+        self.setWindowTitle(self.title)
+        # self.show()
 
-
-class MyThreads(QThread):
-    ipSignal = pyqtSignal(list)
-
-    def __init__(self, ip, parent=None):
-        super(MyThreads, self).__init__(parent)
-        self.ip = ip
-
-    def run(self):
-        ip = self.ip
-        find_ips(ip)
-
+# class MyThreads(QThread):
+#     ipSignal = pyqtSignal(list)
+#
+#     def __init__(self, ip, parent=None):
+#         super(MyThreads, self).__init__(parent)
+#         self.ip = ip
+#
+#     def run(self):
+#         ip = self.ip
+#         find_ips(ip)
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    ex = Ui()
-
-
-    print("start time %s" % time.ctime())
+    # app = QApplication(sys.argv)
+    # ex = Ui('Ping IP')
+    # print('start time {}'.format(time.ctime()))
     ip_prefix = '192.168.22'
     find_ips(ip_prefix)
+    # print('end time {}'.format(time.ctime()))
+    time.sleep(10)
     ip_pool = ip_addresses
-    print(ip_pool)
-    print("end time %s" % time.ctime())
-    sys.exit(app.exec_())
+    print(len(ip_pool), ip_pool)
+    check_ips(ip_addresses)
+    print(ping_result_data, ping_result_time)
+    # sys.exit(app.exec_())
 
