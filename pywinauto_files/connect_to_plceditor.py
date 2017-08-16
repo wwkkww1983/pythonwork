5ae487efa7a4e5955efdc8a6723507e70a7c16de
fanch-luck <fch.luck@qq.com>
2017-08-16 18:45:00 +0800

    后台数据显示优化，PLCEditor调试

--- pywinauto_files/connect_to_plceditor.py
+++ pywinauto_files/connect_to_plceditor.py
@@ -18,22 +18,26 @@ def connect(title_name):
     return app
 
 
-def getwindow(app):
-    alive = False
-    mwindow = app.window(title=r'Wecon PLC Editor - 软元件监视-1')
+def getwindow(app, window_title=r'Wecon PLC Editor'):
+    result = False
+    mwindow = app.window(title=window_title)
     if mwindow.exists(timeout=3):
-        alive = True
+        result = True
         mwindow.print_control_identifiers()
     else:
         pass
-    return alive
+    return 'return {1} while trying to find window or dialog: "{1}" '.format(result, window_title)
+
+
+def modifydata():
+    modi_window = findwindows.find_window(title=r'修改软元件值')
+    print(modi_window)
 
 
 if __name__ == '__main__':
     # app_path = r'D:\Program Files\WECONSOFT\PLC Editor\1.3.3 3VM支持\PLCEditor.exe'
     # target_app = open(app_path)
-
     target_app = connect(r'Wecon PLC Editor - 软元件监视-1')
     time.sleep(3)
-    flag = getwindow(target_app)
+    flag = getwindow(target_app, r'修改软元件值')
     print(flag)
