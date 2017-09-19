b84b6ffffe9f5a60288a603cf10e5bd66f8267ff
fanch-luck <fch.luck@qq.com>
2017-09-19 17:46:45 +0800

    get_HMI.py 更新，支持firefox

--- local_projects/pil_module/pil_thumnail.py
+++ local_projects/pil_module/pil_thumnail.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python
 # _*_ coding: utf-8 _*_
 
-image_path = r'C:\Users\fan\OneDrive\pythonwork\local_projects\pil_module\BingWallpaper-2017-03-23.jpg'
+image_path = r'BingWallpaper-2017-03-23.jpg'
 from PIL import Image
 
 
@@ -11,7 +11,7 @@ def create_small(im_path, im_size):
     im = Image.open(im_path, 'r')
     im_attribute = [im.format, im.size, im.mode]
     print(im_attribute)
-    im.thumbnail(im_size, resample = 3)
+    im.thumbnail(im_size, resample=3)
     # 关于图片质量参数的说明（由于与目前学习内容无相关性，不深究）http://www.youdaili.net/python/13185.html
     im.save('3.jpg', 'JPEG')
 image_size = (400,400)
--- selenium_/get_HMI.py
+++ selenium_/get_HMI.py
@@ -2,7 +2,6 @@
 # !/usr/bin/env python
 # _*_ coding: utf-8 _*_
 
-from selenium import webdriver
 import time
 import base64
 from bs4 import BeautifulSoup
@@ -11,38 +10,36 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 # from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions
 
 
 def open_project(httppath):
     """打开工程网页"""
-    ie_webdriver_server_path = r'C:\Python\Python34\IEDriverServer.exe'
-    browser = webdriver.Ie(ie_webdriver_server_path)
+    browser = webdriver.Firefox()
     browser.get(httppath)
     return browser
 
 
 def make_filelist(proj):
     elem = WebDriverWait(proj, 10).until(
-        expected_conditions.presence_of_element_located((By.ID, "NODE_0DSPL_20")))
+        expected_conditions.presence_of_element_located((By.ID, "NODE_0DSPL_19")))
 
     # print(elem)
     svg = elem.get_attribute('src')
-    #print('svg\n', svg)
+    # print('svg\n', svg)
     head = len('data:image/svg+xml;base64,')
-    #print('attitude head len:\n', head)
+    # print('attitude head len:\n', head)
     svgfile = svg[head:]
-    #print('svgfile:\n', svgfile)
+    # print('svgfile:\n', svgfile)
     svgg = base64.b64decode(svgfile)
-    #print('svggg:\n', svgg)
+    # print('svggg:\n', svgg)
     string = svgg.decode()
-    #print('string\n', string)
+    # print('string\n', string)
     soup = BeautifulSoup(svgg, "html.parser")
-    #print('soup\n', soup)
+    # print('soup\n', soup)
     gs = soup.find_all('text')
     print(gs)
-
+    for i in gs:
+        print(i)
 if __name__ == "__main__":
     project = open_project(r"http://192.168.22.61/")
     time.sleep(3)
