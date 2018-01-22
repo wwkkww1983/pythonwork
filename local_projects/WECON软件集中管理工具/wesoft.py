b1b1b5784840418a63eb7c3a76bae904ac5eb2c6
fanch-luck <fch.luck@qq.com>
2018-01-22 18:13:20 +0800

    sjfile.py 完善

---
+++ local_projects/WECON软件集中管理工具/wesoft.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# -----------------------------------------------------------
+# File Name: wesoft
+# Author:    fan
+# date:      2018/1/24
+# -----------------------------------------------------------
+
+
+class WeSoft(object):
+    def __init__(self):
+        self.soft_name = None
+        self.version = None
+        self.path = None
+        self.flag_file_name = None
+        self.package_size = None
+        self.installed_size = None
+        self.is_in_system_driver = False
+        self.is_work_well = False
