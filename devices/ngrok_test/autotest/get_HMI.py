d9aadcbc0474d53786205b75d713ac2d3fc40402
fanch-luck <fch.luck@qq.com>
2018-02-28 18:47:12 +0800

    测试报告的实现和优化

--- devices/ngrok_test/autotest/get_HMI.py
+++ devices/ngrok_test/autotest/get_HMI.py
@@ -28,9 +28,9 @@ def open_browser():
 def open_project(brsr, httppath):
     """打开工程网页"""
     try:
-        for i in range(5):
+        for i in range(3):
             brsr.get(httppath)
-            time.sleep(2)
+            time.sleep(3)
             try:
                 text = brsr.find_element_by_xpath("/html/body/pre")
                 if 'not found' not in text.text:
@@ -40,27 +40,26 @@ def open_project(brsr, httppath):
             except:
                 break
     except Exception as e:
-        log.error('open remote hmi webpage fail: {}'.format(httppath))
-        print(e)
+        log.error('open remote hmi webpage fail: {}, {}'.format(httppath, e))
         return None
     return brsr
 
 
 def check_hmi(proj):
+    # 尝试查找页面元素并返回用于判断页面存在的信息
     hmi_alive = False
     hmi_date = '-'
     hmi_time = '-'
     check_info = '-'
-    elem = None
     if not proj:
+        # 无法获取目标页面
         check_info = 'fail to open the url'
         log.info(check_info)
         print(proj)
-        # 无法获取目标页面
         return hmi_alive, hmi_date, hmi_time, check_info
     else:
         try:
-            elem = WebDriverWait(proj, 5).until(
+            elem = WebDriverWait(proj, 10).until(
                 expected_conditions.presence_of_element_located((By.XPATH, "/html/body/div")))
             if not elem:
                 pass
@@ -73,13 +72,13 @@ def check_hmi(proj):
                         div_scrnos = {}
                         span_ids = {}
                         span_texts = {}
-                        div_id = ''
+                        # div_id = ''
                         s = 1
                         for i in range(len(divs)):
                             div_ids[i] = divs[i].get_attribute('id')
                             div_scrnos[i] = divs[i].get_attribute('scrno')
                             if div_scrnos[i]:
-                                div_id = div_ids[i]
+                                # div_id = div_ids[i]
                                 break
                             # print(i, div_ids[i], div_scrnos[i])
                             s += 1
@@ -95,13 +94,11 @@ def check_hmi(proj):
                             # print(i, span_ids[i], span_texts[i])
                 except Exception as e:
                     # 查找第一个div元素失败 = HMI未在线
-                    check_info = 'fail to check out date/time part'
-                    # log.info(check_info)
-                    print(e)
+                    check_info = 'date part not found'
+                    # log.error(check_info)
         except Exception as e:
             # 无法获得对应HMI的网页，退出当前连接
-            check_info = 'fail to check out element id= div, fail to link'
-            print(e)
+            check_info = 'div not found'
             # log.error(check_info)
             return hmi_alive, hmi_date, hmi_time, check_info
     return hmi_alive, hmi_date, hmi_time, check_info
--- devices/ngrok_test/autotest/main.py
+++ devices/ngrok_test/autotest/main.py
@@ -32,38 +32,61 @@ def get_hmiurls(xlspath):
     """
     hmiinfo_xls = xlrd.open_workbook(xlspath)
     sheet = hmiinfo_xls.sheet_by_name('设备信息')
+    time.sleep(.5)
+    s=sheet.cell_value(2, 5)
+    n=sheet.nrows
     hmi_urls = []
-    for i in range(2, 19):
+    for i in range(2, sheet.nrows):
+        print(sheet.cell_value(i, 5))
         hmi_urls.append(str(sheet.cell_value(i, 5)))
-    return hmi_urls
+    if hmi_urls:
+        return hmi_urls
 
 
-def create_report(urls):
+def create_report(report_path, urls):
     hmireport_xls = xlwt.Workbook()
     # 必须标记单元格覆盖填写为允许，否则运行报错：Exception: Attempt to overwrite cell
     report_sheet = hmireport_xls.add_sheet('test report', cell_overwrite_ok=True)
-    report_sheet.write(0, 0, 'Ngrok问题网络切换PMI连接检测')
-    sheet_head = ['ID',
-                  'HMI_Name',
-                  'g4router0_SUCS',
-                  'g4router0_Fail',
-                  'router0_SUCS',
-                  'router0_FAIL',
-                  'Switch_SUCS',
-                  'Switch_FAIL',
-                  'Total_n',
-                  'Fail_n',
-                  'URL']
+    # 定义用于存放数据的表（二维列表）
     data_area = []
-    for i in range(len(sheet_head)):
-        # 表格头部
-        report_sheet.write(1, i, sheet_head[i])
-    for line_id, url in zip([nm for nm in range(1,len(urls)+1)], urls):
-        # 组装初始数据
-        row_value = [line_id, url[-28:-24], 0, 0, 0, 0, 0, 0, 0, 0, url]
-        data_area.append(row_value)
-        for n in range(len(row_value)):
-            report_sheet.write(line_id+1, n, row_value[n])
+    if os.path.exists(report_path):
+        # 如果该报告文件存在，在拷贝到内存中，新文件获取该数据并进行初始化
+        e_report_xls = xlrd.open_workbook(report_path)
+        e_report_sheet = e_report_xls.sheet_by_index(0)
+        for i in range(e_report_sheet.nrows):
+            if i >= 2:
+                data_area.append(e_report_sheet.row_values(i))
+            for j in range(e_report_sheet.ncols):
+                report_sheet.write(i, j, e_report_sheet.cell_value(i, j))
+
+    else:
+        # 如果报告文件不存在，进行配置初始化
+        report_sheet.write(0, 0, 'Ngrok问题网络切换PMI连接检测')
+        sheet_head = ['ID',
+                      'HMI_Name',
+                      'G4router0_SUCS',
+                      'G4router0_Fail',
+                      'Router0_SUCS',
+                      'Router0_FAIL',
+                      'Switch_SUCS',
+                      'Switch_FAIL',
+                      'Total_n',
+                      'Fail_n',
+                      'URL']
+
+        for i in range(len(sheet_head)):
+            # 表格头部
+            report_sheet.write(1, i, sheet_head[i])
+        for hmi_id, hmi_url in zip([nm for nm in range(1, len(urls)+1)], urls):
+            # 组装初始数据
+            # hmi_name = hmi_url    # 局域网访问
+            hmi_name = hmi_url[-28:-24]    # 远程访问
+            row_value = [hmi_id, hmi_name, 0, 0, 0, 0, 0, 0, 0, 0, hmi_url]
+            data_area.append(row_value)
+            for n in range(len(row_value)):
+                report_sheet.write(hmi_id+1, n, row_value[n])
+    hmireport_xls.save('Ngrok问题测试报告.xls')
+    time.sleep(2)
     return hmireport_xls, report_sheet, data_area
 
 
@@ -77,30 +100,31 @@ def set_report(data, sht, data_area):
     """
     for i in range(len(data_area)):
         if data[0] == data_area[i][1]:
+            # 第8列-总次数；第9列-错误次数
             data_area[i][8] += 1
-            data_area[i][9] += 1
             if data[1] == 'g4router0':
-                if data[2] == 'True':
+                if data[2] is True:
                     data_area[i][2] += 1
-                if data[2] == 'False':
+                if data[2] is False:
                     data_area[i][3] += 1
                     data_area[i][9] += 1
             if data[1] == 'router0':
-                if data[2] == 'True':
+                if data[2] is True:
                     data_area[i][4] += 1
-                if data[2] == 'False':
+                if data[2] is False:
                     data_area[i][5] += 1
                     data_area[i][9] += 1
             if data[1] == 'switch0':
-                if data[2] == 'True':
+                if data[2] is True:
                     data_area[i][6] += 1
-                if data[2] == 'False':
+                if data[2] is False:
                     data_area[i][7] += 1
                     data_area[i][9] += 1
 
     for k in range(len(data_area)):
         for j in range(len(data_area[k])):
-            sht.write(k+2, j+2, data_area[k][j])
+            if 10 >= j >= 2:
+                sht.write(k+2, j, data_area[k][j])
 
 
 def get_port(p_name='com1', p_baud=9600, p_bysz=8, p_stpb=1, p_prt='N', tmot=1):
@@ -134,13 +158,16 @@ def open_port(port):
 
 def get_each_hmi_status(browser, hmiurls, curdevice, reportsheet, dataarea):
     for url in hmiurls:
-        hmitempname = url
+        hmitempname = url[-28:-24]
         hmi = open_project(browser, url)
         time.sleep(1)
-        hmialive, hmidate, hmitime, checkinfo = check_hmi(hmi)
-        set_report([hmitempname, curdevice, hmialive, hmidate, hmitime, checkinfo],
-                   reportsheet,
-                   dataarea)
+        if hmi:
+            hmialive, hmidate, hmitime, checkinfo = check_hmi(hmi)
+            set_report([hmitempname, curdevice, hmialive, hmidate, hmitime, checkinfo],
+                       reportsheet,
+                       dataarea)
+        else:
+            hmialive, hmidate, hmitime, checkinfo = [False, None, None, 'url not found']
         log.info('[{}], [{}], [{}], [{}], [{}], [{}]'.format(hmitempname,
                                                              curdevice,
                                                              hmialive,
@@ -175,25 +202,28 @@ def set_device_power(prt, pwcd):
     time.sleep(.5)
     close_port(prt)
 
-
 def main_no_plc():
     # 初始化
     log.info('hmi checking format: [device] [hmi name] [alive] [date str] [time str] [info]')
     powercodes = ['11110000', '11101000', '11100100']
-    code_device_map = {'11110000': 'switch0:', '11101000': 'g4router0', '11100100': 'router0'}
+    code_device_map = {'11110000': 'switch0', '11101000': 'g4router0', '11100100': 'router0'}
     p = get_port(p_name='com2',
                  p_baud=9600,
                  p_bysz=7,
                  p_stpb=1,
                  p_prt='E')
     close_port(p)
-    # urls = get_hmiurls('ngrok测试用例设备信息.xls')
-    urls = ['192.168.35.223', '192.168.35.224', ]
-    for i in range(len(urls)):
-        urls[i] = 'http://' + urls[i]
-
-    rept_xls, rept_sht, dt_ar = create_report(urls)
-
+    # 设置HMI监控类型：
+    # 远程访问地址
+    urls = get_hmiurls('ngrok测试用例设备信息.xls')
+    """
+    # 局域网方位地址
+    # urls = ['192.168.35.223', '192.168.35.224', ]
+    # for i in range(len(urls)):
+    #     urls[i] = 'http://' + urls[i]
+    """
+    report_path = 'Ngrok问题测试报告.xls'
+    rept_xls, rept_sht, dt_ar = create_report(report_path, urls)
     log.info('checking remote hmi status')
 
     # 开始测试
@@ -202,9 +232,9 @@ def main_no_plc():
     time.sleep(3)
     while True:
         # 使前两种方式交替，实现三台设备两两切换12，23，31，32，21，13
-        temp = powercodes[0]
-        powercodes[0] = powercodes[1]
-        powercodes[1] = temp
+        # temp = powercodes[0]
+        # powercodes[0] = powercodes[1]
+        # powercodes[1] = temp
         for powcode in powercodes:
             times += 1
             log.info('current powercode:{}, net swtich times:{}'.format(powcode, times))
@@ -215,10 +245,8 @@ def main_no_plc():
             t.setDaemon(True)
             t.start()
             t.join()
-
-        # browser.quit()
-        rept_xls.save('Ngrok问题测试报告.xls')
-
+        rept_xls.save(report_path)
+        time.sleep(5)
         log.info('powercode {} checking finished'.format(powercodes))
 
 
@@ -226,15 +254,26 @@ def main():
     # 初始化
     log.info('hmi checking format: [device] [hmi name] [alive] [date str] [time str] [info]')
     powercodes = ['11110000', '11101000', '11100100']
-    code_device_map = {'11110000': 'stitch0:', '11101000': 'g4router0', '11100100': 'router0'}
+    code_device_map = {'11110000': 'switch0', '11101000': 'g4router0', '11100100': 'router0'}
     p = get_port(p_name='com2',
                  p_baud=9600,
                  p_bysz=7,
                  p_stpb=1,
                  p_prt='E')
     close_port(p)
+    # 设置HMI监控类型：
+    # 远程访问地址
     urls = get_hmiurls('ngrok测试用例设备信息.xls')
+    """
+    # 局域网访问地址
+    # urls = ['192.168.35.223', '192.168.35.224', ]
+    # for i in range(len(urls)):
+    #     urls[i] = 'http://' + urls[i]
+    """
+    report_path = 'ngrok测试用例设备信息.xls'
+    rept_xls, rept_sht, dt_ar = create_report(report_path, urls)
     log.info('checking remote hmi status')
+
     # 开始测试
     times = 0
     browser = open_browser()
@@ -253,7 +292,7 @@ def main():
             t.setDaemon(True)
             t.start()
             t.join()
-        # browser.quit()
+        rept_xls.save(report_path)
         log.info('powercode {} checking finished'.format(powercodes))
 
 
