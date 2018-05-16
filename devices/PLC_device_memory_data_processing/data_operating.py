14d33d4586caa07ed7c16daaf2c9f3137a8e8650
fanch-luck <fch.luck@qq.com>
2018-05-16 21:20:45 +0800

    PLCEditor导出软元件内存D寄存器采样数据统计工具 优化 支持同格式数据的批量整理

--- devices/PLC_device_memory_data_processing/data_operating.py
+++ devices/PLC_device_memory_data_processing/data_operating.py
@@ -47,31 +47,31 @@ def read_and_write(dirpath, d_start, d_end, x_label, y_label, save_path):
 
         for i, x, y in zip(range(2, len(xdata)+2), xdata, ydata):
             # 数据写入sheet,注意表头部写标题和标签，不要覆盖
-            if y and y != '0':
-                sheet.write(i, 0, x)
-                sheet.write(i, 1, int(y))
+            # if y and y != '0':
+            sheet.write(i, 0, x)
+            sheet.write(i, 1, int(y))
 
     tarketbook.save(save_path)
 
 
-def read_data(readpath, begind, endd):
-    book = xlrd.open_workbook(readpath)
-    table = book.sheets()[0]
-
-    ds = table.col_values(0)
-    ds = list(ds)
-    begin = ds.index(begind)
-    end = ds.index(endd)
-    data = []
-    for i in range(begin, end):
-        row_data = table.row_values(i)
-        row_data.remove(row_data[0])
-        data = data + row_data
-
-    ydata = data
-    xdata = list(range(1, len(data)))
-
-    return xdata, ydata
+# def read_data(readpath, begind, endd):
+#     book = xlrd.open_workbook(readpath)
+#     table = book.sheets()[0]
+#
+#     ds = table.col_values(0)
+#     ds = list(ds)
+#     begin = ds.index(begind)
+#     end = ds.index(endd)
+#     data = []
+#     for i in range(begin, end):
+#         row_data = table.row_values(i)
+#         row_data.remove(row_data[0])
+#         data = data + row_data
+#
+#     ydata = data
+#     xdata = list(range(1, len(data)))
+#
+#     return xdata, ydata
 
 
 def write_data(savepath, xdata, ydata, xlabel='时间/?s', ylabel='温度/0.1℃', title='sheet1'):
@@ -91,10 +91,11 @@ def write_data(savepath, xdata, ydata, xlabel='时间/?s', ylabel='温度/0.1℃
         print(e)
 
 if __name__ == '__main__':
-    dir_path = r'E:\MyWorkPlace\pythonwork\devices\PLC_device_memory_data_processing'
+    dir_path = r'E:\MyWorkPlace\pythonwork\devices\PLC_device_memory_data_processing\xlses'
     sav_pth = r"E:\MyWorkPlace\pythonwork\devices\PLC_device_memory_data_processing\已处理表格.xls"
+    # Dxxxx只能是出现在软元件内存文件中索引的D地址
     stt = ['D1000']
-    ed = ['D1296']
+    ed = ['D7000']
     x_lab = '采样周期/*ms'
     y_lab = '温度/*℃'
     read_and_write(dir_path, stt, ed, x_lab, y_lab, sav_pth)
---
+++ devices/PLC_device_memory_data_processing/getfiles.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+遍历指定目录下的所有子目录和文件，并返回所有文件的完整文件路径
+"""
+
+import os
+
+
+def get_files(given_dir):
+    """定义函数获取给定目录下的所有文件名（不包含目录名），并添加到列表中"""
+    files = []
+
+    def findfiles(current_dir):
+        """定义函数遍历所有子目录"""
+        for each_path in os.listdir(current_dir):
+            each_abspath = os.path.join(current_dir, each_path)
+            """遍历当前一层目录并组装为绝对路径"""
+
+            if os.path.isfile(each_abspath):
+                """如果是文件，添加文件完整路径到列表中"""
+                files.append(each_path)
+            else:
+                """如果是目录，则什么都不做"""
+                pass
+
+    findfiles(given_dir)
+    return files
+
+if __name__ == '__main__':
+    test_path = r'E:\MyWorkPlace\pythonwork\devices\PLC_device_memory_data_processing'
+    l = get_files(test_path)
+    print('\n在目录：', test_path, '\n存在的全部xls文件为：')
+    for i in l:
+        if i[-3:] == 'xls':
+            print(i)
+
+
+
+
