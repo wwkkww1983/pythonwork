83fe61b033c7ba4c00740dafef0dcca2cafc011d
fanch-luck <fch.luck@qq.com>
2018-05-23 18:15:46 +0800

    找到sign_error问题原因：sored(dict),在py3中要按默认方式保证comid在compvtvalue之前

--- devices/vbox_auto_test/web_api.py
+++ devices/vbox_auto_test/web_api.py
@@ -7,6 +7,7 @@
 # -----------------------------------------------------------
 import os
 import requests as req
+from urllib.parse import urlencode
 import json
 import hashlib
 import time
@@ -15,6 +16,9 @@ debug = True
 url_test = "http://192.168.45.186:8686/box-data/api/"
 url_normal = "http://api.v-box.net/box-data/api/"
 api_login = 'we-data/login'
+api_boxes_list = 'we-data/boxs'
+nowtime = lambda: int(round(time.time()*1000))    # 当前时间戳单位ms
+# lambda表达式，冒号前表示匿名函数传入参数，冒号后表示匿名函数返回值
 keyvalue="key=f1cd9351930d4e589922edbcf3b09a7c"    # headers参数，特定值，研发提供（猜测跟公司绑定）
 
 common_para_dic = {"comid": "1",
@@ -25,26 +29,31 @@ common_para_dic = {"comid": "1",
 
 login_data_dic = {"alias": "test_fan",
                   "password": "123456",
-                  # "isremember": 1
+                  "isremember": 0
                   }
-headers_without_common = {"Host": "v-box.net",
-                          "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:48.0) Gecko/20100101 Firefox/48.0",
-                          # "Accept": "*/*",
-                          # "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
-                          # "Accept-Encoding": "gzip, deflate",
+headers_without_common = {"Host": '192.168.45.186:8686',
+                          "User-Agent": 'Mozilla/5.0 (Windows NT 6.1; WOW64) '
+                                        'AppleWebKit/537.36 (KHTML, like Gecko) '
+                                        'Chrome/31.0.1650.63 '
+                                        'Safari/537.36',
                           "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-                          # "Referer": "http://www.v-box.net/web/html/user/login.html",
-                          # "Origin": "http://www.v-box.net",
                           "Connection": "keep-alive"
                           }
-nowtime = lambda: int(round(time.time()*1000))    # 当前时间戳单位ms
-# lambda表达式，冒号前表示匿名函数传入参数，冒号后表示匿名函数返回值
 
 
 def cal_md5(string):
     md5 = hashlib.md5()
     md5.update(string.encode("utf8"))
     return md5.hexdigest()
+
+
+def sort_dict(dic):
+    return sorted_lis
+
+
+def url_encode(lis, dic):
+    return encoded_url
+
 
 
 def sign(para_dict: dict):
@@ -55,14 +64,15 @@ def sign(para_dict: dict):
     :return:所有参数的MD5校验值
     """
     para_dict['ts'] = nowtime()    # ts动态更新
-    sorted_dic_list = sorted(para_dict, key=lambda x: x[0], reverse=False)    # 返回已排序字典，排序依据=每个key的首元素，升序
-    # sorted_dic_list = sorted(para_dict)    # 按sorted默认方式排序即可
-    # print(sorted_dic_list)
+    # sorted_dic_list = sorted(para_dict, key=lambda x: x[0], reverse=False)    # 返回已排序字典，排序依据=每个key的首元素，升序
+    # 注意！！！上下两种方式排列结果不同，应按照下面的方式
+    sorted_dic_list = sorted(para_dict)    # 按sorted默认方式排序即可
     para_str = ''
     for key in sorted_dic_list:
         para_str += '{k}={v}&'.format(k=key, v=para_dict[key])
     para_str += keyvalue  # 待算md5的字符串准备完毕
-    # print(para_str)
+    if debug:
+        print('str to be signed: ', para_str)
     para_str_md5_value = cal_md5(para_str)
     # print(para_str_md5_value)
     return para_str_md5_value
@@ -98,10 +108,11 @@ def do_login():
         print("newdict: {nd}\n\n"
               "common_para_dict2: {cpd}\n\n"
               "headers: {hds}\n".format(nd=new_login_dict, cpd=common_para_dic2, hds=headers_dict))
-    r = req.post(url, data=new_login_dict, headers=headers_dict)\
 
+    r = req.post(url, data=new_login_dict, headers=headers_dict)
     login_response_dict = r.json()
     if debug:
+        print('data:\n', urlencode(new_login_dict))
         print('login_response_dict')
         for k, v in login_response_dict.items():
             print("   ", k, "=", v)
@@ -110,6 +121,9 @@ def do_login():
             print("   ", k, "=", v)
     return login_data_dic
 
+
+def get_vboxes_list():
+    pass
 
 
 if __name__ == '__main__':
