963183f14ce818516835b1ba989d3335bb3891aa
fanch-luck <fch.luck@qq.com>
2018-07-16 21:25:57 +0800

    开始实现，验证公司局域网版本设置异同与程序能否正常访问的关系

--- devices/redmine/redmine_practice.py
+++ devices/redmine/redmine_practice.py
@@ -8,15 +8,44 @@
 
 from redminelib import Redmine
 
-url = 'http://192.168.0.110/redmine'
-user = 'yonghuer'
-passwd = '111111111'
-redmine = Redmine(url, username=user, password=passwd)
-project = redmine.project.get('xiangmuyi')    # 必须是'项目标识'
-for resouce in list(project):
-    print(resouce[0], ':', resouce[1])
-issues = project.issues
-issue = issues[0]
-print('\n\n问题/任务数量: {}\n\n'.format(len(issues)))
-for resouce in list(issue):
-    print(resouce[0], ':', resouce[1])
+
+def visit_redmine():
+    url = 'http://192.168.39.40/redmine'
+    user = 'yonghuer'
+    passwd = '111111111'
+    redmine = Redmine(url, username=user, password=passwd)
+    project = redmine.project.get('xiangmuyi')    # 必须是'项目标识'
+    for resouce in list(project):
+        print(resouce[0], ':', resouce[1])
+    issues = project.issues
+    issue = issues[0]
+    print('\n\n问题/任务数量: {}\n\n'.format(len(issues)))
+    for resouce in list(issue):
+        print(resouce[0], ':', resouce[1])
+
+
+class Project(object):
+    def __init__(self, identifier, url, user, passwd):
+        self.identifier = identifier
+        redmine = Redmine(url, username=user, password=passwd)
+        self.project = redmine.project.get(self.identifier)
+        self.print_project_info()
+
+    def print_project_info(self):
+        for resouce in sorted(list(self.project)):
+            print(resouce[0], ':', resouce[1])
+        print('\n\n问题/任务数量: {}\n\n'.format(len(self.project.issues)))
+
+
+if __name__ == "__main__":
+    # urll = "http://192.168.11.118:7777/redmine/"
+    # usern = "fanchunhui"
+    # passw = "a6361255"
+    # identifierr = 'testgroup'
+
+    urll = "http://192.168.39.40/redmine"
+    usern = "yonghuyi"
+    passw = "11111111"
+    identifierr = "xiangmuyi"
+
+    project = Project(identifierr, urll, usern, passw)
