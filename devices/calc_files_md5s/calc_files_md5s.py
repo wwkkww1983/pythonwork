a433c254ed2c0006888943ff87aa938aee9b05a4
fanch-luck <fch.luck@qq.com>
2018-09-27 19:04:42 +0800

    不同场景下的MD5校验实现

---
+++ devices/calc_files_md5s/calc_files_md5s.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# -----------------------------------------------------------
+# File Name: calc_files_md5s
+# Author:    fan
+# date:      2018/9/27
+# -----------------------------------------------------------
+# 递归计算压缩包或文件夹内文件的MD5值
+import hashlib
+import os
+import logging as log
+import rarfile, zipfile
+
+log.basicConfig(level=log.ERROR,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s: %(message)s')
+
+
+def cal_md5(fileb, nbyte=1024):
+    # 针对‘file'类型输入
+    md5 = hashlib.md5()
+    while True:
+        current_bytes = fileb.read(nbyte)
+        if current_bytes:
+            md5.update(current_bytes)
+        else:
+            fileb.close()
+            break
+    return md5.hexdigest()
+
+
+def defile(filepath:str):
+    try:
+        f = open(filepath, 'rb')
+        return {filepath: f}
+    except Exception as e:
+        log.error(e)
+        return {}
+
+
+def defolder(folderpath:str):
+    name_file_dict = {}
+    os.chdir(folderpath)
+    for root, dirs, files in os.walk(folderpath):
+        for i in files:
+            fp = os.path.join(root, i)
+            fb = open(fp, 'rb')
+            fpp = fp[len(folderpath):]    # 截去相对路径
+            name_file_dict[fpp] = fb   # 组装相对路径和文件流为字典
+    log.info(name_file_dict)
+    return name_file_dict
+
+
+
+
+def dezip(zippath:str):
+    name_file_dict = {}
+    zip = zipfile.ZipFile(zippath)
+    for name in zip.namelist():
+        fb = zip.open(name, 'rU')    # f为byte类型
+        name_file_dict[name] = fb
+    return name_file_dict
+
+
+def derar(zippath:str):
+    name_file_dict = {}
+    rar = rarfile.RarFile(zippath)
+    for name in rar.namelist():
+        fb = rar.open(name, 'rU')    # f为byte类型
+        name_file_dict[name] = fb
+    return name_file_dict
+
+
+def calc_files_md5s(name_file_dic:dict):
+    md5s = {}
+    for key in name_file_dic:
+        md5s[key] = cal_md5(name_file_dic[key])
+    # log.info(md5s)
+    return md5s
+
+
+def main(import_type:str, path:str):
+    md5s = None
+    if not import_type:
+        log.error('please select the right import type, then try again.')
+    else:
+        if import_type is 'string':
+            pass
+        else:
+            if not os.path.exists(path):
+                log.error('please select the right file or folder path, then try again.')
+            else:
+                if import_type is 'file':
+                    nd = defile(path)
+                    md5s = calc_files_md5s(nd)
+                elif import_type is 'folder':
+                    nd = defolder(path)
+                    md5s = calc_files_md5s(nd)
+                elif import_type is 'zip':
+                    nd = dezip(path)
+                    md5s = calc_files_md5s(nd)
+                elif import_type is 'rar':
+                    nd = derar(path)
+                    md5s = calc_files_md5s(nd)
+                # elif import_type is '7z':    # 暂不支持
+                #     pass
+                else:
+                    log.error('the selected file is not supported, please check.')
+    # log.info(md5s)
+    print('\n', import_type, path)
+    for k in sorted(md5s.keys()):
+        print('{}, {}'.format(k, md5s[k]))
+    return md5s
+
+if __name__ == '__main__':
+    main('file', 'productfile.osf')
+    main('zip', 'productfile.osf')
+    main('folder', '.')
