84423fec1da4f82c87b37823acf61a0860b83b06
fanch-luck <fch.luck@qq.com>
2019-09-04 10:33:29 +0800

    利用Python unittest自带测试框架执行测试-命令行运行

--- devices/vbox_auto_test/vnet_http_api_test/test_signin.py
+++ devices/vbox_auto_test/vnet_http_api_test/test_signin.py
@@ -5,56 +5,76 @@
 # Author:    fan
 # date:      2019/8/12 012
 # -----------------------------------------------------------
-from vnet_http_api_cls import *
-import unittest
+# from devices.vbox_auto_test.vnet_http_api_test.vnet_http_api_cls import *
 import HTMLTestRunner
+
+from vnet_http_api_cls import VnetHttpApi
+import unittest
 
 
 class TestVnetSignin(unittest.TestCase):
     def setUp(self):
-        # 测试对象初始化
-        self.vnet = VnetHttpApi()
+        """
+        测试环境初始化，适用于每个用例开始执行前
+        :return:
+        """
+        self.vnet = VnetHttpApi("api.v-box.net")
+        self.api_signin = 'we-data/login'
+        self.data_signin = {
+            "alias": "",
+            "password": "",
+            "isremeber": 0  # 文档中即如此拼写
+        }
 
     def tearDown(self):
-        # 测试用例结束
+        """
+        测试环境清理，适用于每个用例执行完毕后
+        :return:
+        """
         self.vnet = None
 
     def test_signin_correct(self):
-        # 测试登录接口, 帐号密码正确
-        data_signin["alias"] = "test_fan"
-        data_signin["password"] = "123456"
-        r = self.vnet.post(api_signin, data_signin, '')
+        """
+        测试登录接口, 帐号密码正确
+        :return:
+        """
+        self.data_signin["alias"] = "test_fan"
+        self.data_signin["password"] = "123456"
+        r = self.vnet.vnet_post(self.api_signin, self.data_signin, '')
         self.assertEqual(r["code"], 200)
 
     def test_signin_password_error(self):
-        # 测试登录接口, 密码错误
-        data_signin["alias"] = "test_fan"
-        data_signin["password"] = "12345*"
-        r = self.vnet.post(api_signin, data_signin, '')
+        """
+        测试登录接口, 密码错误
+        :return:
+        """
+        self.data_signin["alias"] = "test_fan"
+        self.data_signin["password"] = "12345*"
+        r = self.vnet.vnet_post(self.api_signin, self.data_signin, '')
         self.assertEqual(r["code"], 11003)
 
     def test_signin_password_empty(self):
-        # 测试登录接口, 密码为空
-        data_signin["alias"] = "test_fan"
-        data_signin["password"] = ""
-        r = self.vnet.post(api_signin, data_signin, '')
+        """
+        测试登录接口, 密码为空
+        :return:
+        """
+        self.data_signin["alias"] = "test_fan"
+        self.data_signin["password"] = ""
+        r = self.vnet.vnet_post(self.api_signin, self.data_signin, '')
         self.assertEqual(r["code"], 11003)
 
 
 if __name__ == '__main__':
-    # unittest.main()
-    # suite = unittest.TestSuite(map(TestVnetSignin, ["test_signin_correct",
-    #                                                 "test_signin_password_error",
-    #                                                 "test_signin_password_empty"]))
     suite = unittest.TestSuite()
     suite.addTest(TestVnetSignin("test_signin_correct"))
     suite.addTest(TestVnetSignin("test_signin_password_error"))
     suite.addTest(TestVnetSignin("test_signin_password_empty"))
 
-    # report_file = open("v-net api test report.txt", "w")
-    # runner = unittest.TextTestRunner(stream=report_file, verbosity=2)
+    report_file = open(u"慧网登录接口测试报告.txt", "w")
+    runner = unittest.TextTestRunner(stream=report_file, verbosity=2)
 
-    report_file = open("v-net api test report.html", "wb")
-    runner = HTMLTestRunner.HTMLTestRunner(stream=report_file, title=u"慧网接口测试报告")
+    # report_file = open(u"慧网登录接口测试报告.html", "wb")
+    # runner = HTMLTestRunner.HTMLTestRunner(stream=report_file, title=u"慧网登录接口测试报告")
+
     runner.run(suite)
     report_file.close()
